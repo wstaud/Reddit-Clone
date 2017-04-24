3649f989af6003e9b2f7c7d0f2de6756ea601705
xconstaud <xconstaud@gmail.com>
2017-04-24 12:33:33 -0500

    Add roll dice exercise

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/sayhello/{name?}', function ($name = "World") {
+    $data = ["name" => $name];
+
+    return view('my-first-view', $data);
+});
+
+Route::get('/rolldice/{guess?}', function ($guess) {
+    $roll = rand(1,6);
+
+    if($guess === $roll){
+        $message = "You guessed it!";
+    }else{
+        $message = "You were wrong";
+    }
+
+    $data = [
+        "guess" => $guess,
+        "roll" => $roll,
+        "message" => $message
+
+    ];
+    return view('roll-dice', $data);
+});
+
+
+
 Route::get('/uppercase/{word}', function ($word) {
     return strtoupper($word);
 });
---
+++ resources/views/roll-dice.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>My First View</title>
+    </head>
+    <body>
+        <h1><?= $message ?></h1>
+        <h1>Your guess:  <?= $guess ?></h1>
+        <h1>Dice Roll: <?= $roll ?></h1>
+    </body>
+</html>
