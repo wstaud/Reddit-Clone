611b4a87658bf9ec4845550274b11a169d3a5e82
xconstaud <xconstaud@gmail.com>
2017-04-27 00:25:25 -0500

    Update tabs to spaces

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,54 +10,54 @@ use App\Http\Controllers\Controller;
 class PostsController extends Controller
 {
 
-  public function index()
-  {
-      $posts = \App\Models\Post::all();
-      return view('/posts/index')->with('posts', $posts);
+    public function index()
+    {
+        $posts = \App\Models\Post::all();
+        return view('/posts/index')->with('posts', $posts);
 
-  }
+    }
 
-  public function create()
-  {
-      return view('/posts/create');
-  }
+    public function create()
+    {
+        return view('/posts/create');
+    }
 
-  public function store(Request $request)
-  {
-      if ($request->has('title')) {
-          // name is not empty
-          $post = new \App\Models\Post();
-          $post->title = $request->title;
-          $post->url = $request->url;
-          $post->content = $request->content;
-          $post->created_by = 1; //TODO: Update this to actual user ID session
-          $post->save();
-      }
-      return redirect('posts');
-  }
+    public function store(Request $request)
+    {
+        if ($request->has('title')) {
+            // name is not empty
+            $post = new \App\Models\Post();
+            $post->title = $request->title;
+            $post->url = $request->url;
+            $post->content = $request->content;
+            $post->created_by = 1; //TODO: Update this to actual user ID session
+            $post->save();
+        }
+        return redirect('posts');
+    }
 
-  public function show($id)
-  {
-		$post = \App\Models\Post::find($id);
-		if ($post === null){
-			return view('404');
-		}else{
-			return view('/posts/show')->with('post', $post);
-		}
-  }
+    public function show($id)
+    {
+    	$post = \App\Models\Post::find($id);
+    	if ($post === null){
+    		return view('404');
+    	}else{
+    		return view('/posts/show')->with('post', $post);
+    	}
+    }
 
-  public function edit($id)
-  {
-      return 'This should show a form for editing a post';
-  }
+    public function edit($id)
+    {
+        return 'This should show a form for editing a post';
+    }
 
-  public function update(Request $request, $id)
-  {
-      return 'This should edit a post';
-  }
+    public function update(Request $request, $id)
+    {
+        return 'This should edit a post';
+    }
 
-  public function destroy($id)
-  {
-      return 'This should remove a post';
-  }
+    public function destroy($id)
+    {
+        return 'This should remove a post';
+    }
 }
